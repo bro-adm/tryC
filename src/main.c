91868441b20a3e35fb3b73b1009b99d1648d180b
bro-adm <lielbondy@gmail.com>
2026-02-11 20:03:10 +0200

    [+] shift-k run
    ### Shift-K - Run ###

--- src/main.c
+++ src/main.c
@@ -1,7 +1,15 @@
 #include <stdlib.h>
+#include <stdio.h>
+
+#include "tryC/shift_k.h"
 
 int main(int argc, char *argv[])
 {
+    char str[] = "a!bc"; // instead of char* str to string literal "whatever" becuase then it would a pointer to the immutable string instead of teh copied mutable string on the stack
+    shift_k(str, -3);
+    printf("%s\n", str);
+
+    printf("%c, %d\n", 'A', 'A');
     return EXIT_SUCCESS;
 }
 
